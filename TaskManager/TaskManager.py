a2a95ff5a0d4ee823de0adbdd9b9535474d4492a
Dre <alekdre001@gmail.com>
2024-10-12 06:24:53 -0900

    Add project files.

---
+++ TaskManager/TaskManager.py
@@ -0,0 +1,18 @@
+from os import path
+
+class TaskManager:  
+ 
+
+    def __init__(self):
+        return 0
+ 
+
+
+def main():
+    app = TaskManager
+
+    app.__init__(app)
+    return 0
+
+
+main()
